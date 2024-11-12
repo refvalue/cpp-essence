2d86e33cf4ef70eb4c2c9381db9c2dd0f74dd9e2
metabeyond <metabeyond@outlook.com>
2024-11-12 23:44:25 +0800

    Reused Actions. Bootstrap Code.

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,7 +1,7 @@
 $ExecutingDirectory = [System.IO.Path]::GetDirectoryName($PSCommandPath)
 Set-Location $ExecutingDirectory
 
-git submodule update --init --recursive
+git submodule update --init
 
 Set-Location "$ExecutingDirectory/third-party/boost-archive-for-cpprestsdk"
 git config --local core.eol lf
