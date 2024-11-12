ec7d047e4ae0ad123a7c289ab864d93ed9588788
metabeyond <metabeyond@outlook.com>
2024-11-12 23:38:59 +0800

    Reused Actions. Bootstrap Code.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 executing_directory=$(dirname "$(realpath "$0")")
-cd "$executing_directory"
+cd "$executing_directory" || exit
 
-git submodule update --init --recursive
+git config --local --unset-all safe.directory "$executing_directory"
+git config --local --add safe.directory "$executing_directory"
+git submodule update --init
 
-cd "$executing_directory/third-party/boost-archive-for-cpprestsdk"
+cd "$executing_directory/third-party/boost-archive-for-cpprestsdk" || exit
 git config --local core.eol lf
 git config --local core.autocrlf false
 git rm --cached -r . && git reset --hard
 
-cd "$executing_directory/third-party/openssl"
+cd "$executing_directory/third-party/openssl" || exit
 git config --local core.eol lf
 git config --local core.autocrlf false
 git rm --cached -r . && git reset --hard
